7dedc3e1e797bb7c963252d0cc7174f7a65570ea
korialuo <koria.luo@foxmail.com>
2016-10-06 18:21:55 +0800

    fix lua csv lib, use lpeg wrap instead of c lib.

--- test/testcsv.lua
+++ test/testcsv.lua
@@ -1,14 +1,10 @@
 local skynet = require "skynet"
 local csv = require "csv"
 
-local function readfile(filename)
-    local f = csv.open(filename)
-    for fields in f:lines() do
-        for i, v in ipairs(fields) do print(i, v) end
-    end
-end
-
 skynet.start(function()
-    readfile("./test/testdata.csv")
+    local data = assert(csv.fromfile("./test/testdata.csv"))
+    for _, r in ipairs(data) do
+        for k, v in pairs(r) do print(k, v, type(v)) end
+    end
     skynet.exit()
 end)
