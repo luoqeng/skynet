53ac8c2522b8977afbd5584792dd960bd84ac24c
korialuo <koria.luo@foxmail.com>
2016-11-10 00:12:41 +0800

    fix lua-snowflake lib.

--- lualib-src/lua-snowflake.c
+++ lualib-src/lua-snowflake.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 #include <sys/time.h>
 
 #include <lua.h>
 #include <lauxlib.h>
+
+#define LUA_SNOWFLAKE_METATABLE ("__lua_snowflake")
 
 #define MAX_INDEX_VAL       0x0fff
 #define MAX_WORKID_VAL      0x03ff
@@ -18,7 +19,8 @@ typedef struct _t_ctx {
     int16_t index;
 } ctx_t;
 
-static ctx_t* g_ctx = NULL;
+static int g_inited = 0;
+static ctx_t g_ctx;
 
 static int64_t
 get_timestamp() {
@@ -32,61 +34,56 @@ wait_next_msec() {
     int64_t current_timestamp = 0;
     do {
         current_timestamp = get_timestamp();
-    } while (g_ctx->last_timestamp >= current_timestamp);
-    g_ctx->last_timestamp = current_timestamp;
-    g_ctx->index = 0;
+    } while (g_ctx.last_timestamp >= current_timestamp);
+    g_ctx.last_timestamp = current_timestamp;
+    g_ctx.index = 0;
 }
 
 static uint64_t
 next_id() {
-    if (!g_ctx) {
-        return 0;
+    if (!g_inited) {
+        return -1;
     }
-    pthread_mutex_lock(&g_ctx->sync_policy);
+    pthread_mutex_lock(&g_ctx.sync_policy);
     int64_t current_timestamp = get_timestamp();
-    if (current_timestamp == g_ctx->last_timestamp) {
-        if (g_ctx->index < MAX_INDEX_VAL) {
-            ++g_ctx->index;
+    if (current_timestamp == g_ctx.last_timestamp) {
+        if (g_ctx.index < MAX_INDEX_VAL) {
+            ++g_ctx.index;
         } else {
             wait_next_msec();
         }
     } else {
-        g_ctx->last_timestamp = current_timestamp;
-        g_ctx->index = 0;
+        g_ctx.last_timestamp = current_timestamp;
+        g_ctx.index = 0;
     }
     int64_t nextid = (int64_t)(
-        ((g_ctx->last_timestamp & MAX_TIMESTAMP_VAL) << 22) | 
-        ((g_ctx->work_id & MAX_WORKID_VAL) << 12) | 
-        (g_ctx->index & MAX_INDEX_VAL)
+        ((g_ctx.last_timestamp & MAX_TIMESTAMP_VAL) << 22) | 
+        ((g_ctx.work_id & MAX_WORKID_VAL) << 12) | 
+        (g_ctx.index & MAX_INDEX_VAL)
     );
-    pthread_mutex_unlock(&g_ctx->sync_policy);
+    pthread_mutex_unlock(&g_ctx.sync_policy);
     return nextid;
 }
 
-static ctx_t* 
-unique_instance(uint16_t work_id) {
-    if (g_ctx) {
-        return g_ctx;
+static int
+init(uint16_t work_id) {
+    if (g_inited) {
+        return 0;
     }
-    g_ctx = (ctx_t *)malloc(sizeof(ctx_t));
-    if (!g_ctx) {
-        return NULL;
+    g_ctx.work_id = work_id;
+    g_ctx.index = 0;
+    if (pthread_mutex_init(&g_ctx.sync_policy, NULL)) {
+        return -1;
     }
-    g_ctx->work_id = work_id;
-    g_ctx->index = 0;
-    if (pthread_mutex_init(&g_ctx->sync_policy, NULL)) {
-        free(g_ctx);
-        g_ctx = NULL;
-    }
-    return g_ctx;
+    g_inited = 1;
+    return 0;
 }
 
 static void
-destroy_instance() {
-    if (g_ctx) {
-        pthread_mutex_destroy(&g_ctx->sync_policy);
-        free(g_ctx);
-        g_ctx = NULL;
+fini() {
+    if (g_inited) {
+        pthread_mutex_destroy(&g_ctx.sync_policy);
+        g_inited = 0;
     }
 }
 
@@ -100,7 +97,7 @@ linit(lua_State* l) {
         }
         work_id = (int16_t)id;
     }
-    if (!unique_instance(work_id)) {
+    if (init(work_id)) {
         return luaL_error(l, "Init instance error, not enough memory.");
     }
     lua_pushboolean(l, 1);
@@ -116,19 +113,32 @@ lnextid(lua_State* l) {
 
 static int
 lfini(lua_State* l) {
-    destroy_instance();
+    fini();
     return 0;
 }
+
+luaL_Reg mtlib[] = {
+    { "__gc", lfini },
+    { NULL, NULL }
+};
+
+luaL_Reg lib[] = {
+    { "init", linit },
+    { "next_id", lnextid },
+    { NULL, NULL }
+};
 
 int
 luaopen_snowflake(lua_State* l) {
     luaL_checkversion(l);
-	luaL_Reg lib[] = {
-        { "__gc", lfini },
-		{ "init", linit },
-		{ "next_id", lnextid },
-		{ NULL, NULL }
-	};
-	luaL_newlib(l, lib);
-	return 1;
+    if (luaL_newmetatable(l, LUA_SNOWFLAKE_METATABLE)) {
+        lua_pushvalue(l, -1);
+        lua_setfield(l, -2, "__index");
+        luaL_setfuncs(l, mtlib, 0);
+        lua_pop(l, 1);
+    }
+    luaL_newlib(l, lib);
+    luaL_getmetatable(l, LUA_SNOWFLAKE_METATABLE);
+    lua_setmetatable(l, -2);
+    return 1;
 }
